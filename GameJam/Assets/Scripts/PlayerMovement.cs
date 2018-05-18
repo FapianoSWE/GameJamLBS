09f7aa6566266c71c78627144de844d05322c07e
FapianoSWE <33018606+FapianoSWE@users.noreply.github.com>
2018-05-18 19:27:37 +0200

    Basic PlayerMovement
    lade till rörelse för spelaren

---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //Movement Variables
+    public float speed;
+    public float jumpForce;
+    public bool isGrounded;
+    public Vector2 moveDirection;
+
+
+    //Physics
+    Rigidbody2D rb;
+    BoxCollider2D collider;
+
+
+	void Start ()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+	}
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Update ()
+    {
+		
+	}
+    void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if(horizontal < 0 || horizontal > 0)
+        {
+            rb.AddForce(new Vector2(horizontal * speed, 0));
+        }
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(horizontal * speed, jumpForce));
+        }
+    }
+}
