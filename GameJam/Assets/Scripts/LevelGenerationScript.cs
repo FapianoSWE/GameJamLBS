c62692527f22ccb372970fbc3dd9ba976c742c33
Felix Ulvmåne <38222220+Ulvmane@users.noreply.github.com>
2018-05-18 20:07:14 +0200

    Level Gen Done

---
+++ GameJam/Assets/Scripts/LevelGenerationScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerationScript : MonoBehaviour {
+
+    public List<GameObject> LevelModules;
+
+    public int levelLength;
+
+    void Start () {
+        int lastRoom = LevelModules.Count + 1;
+        for (int i = 0; i<levelLength; i++)
+        {
+            int room = Random.Range(0,LevelModules.Count);
+            while (room == lastRoom)
+            {
+                room = Random.Range(0, LevelModules.Count);
+            }        
+            GameObject Room = Instantiate(LevelModules[room], new Vector2(18*i,0), Quaternion.identity);
+            lastRoom = room;
+        }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
