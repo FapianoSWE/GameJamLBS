a4742e9017394cd9572d5532d349140df951a0eb
Felix Ulvmåne <38222220+Ulvmane@users.noreply.github.com>
2018-05-19 08:34:18 +0200

    Enemy

--- GameJam/Assets/Scripts/Enemy/EnemyBehaviourScript.cs
+++ GameJam/Assets/Scripts/Enemy/EnemyBehaviourScript.cs
@@ -4,13 +4,93 @@ using UnityEngine;
 
 public class EnemyBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
+    //Enums
+    public enum EnemyState
+    {
+        patrolling,
+        aggressive,
+    }
+    public EnemyState currentState = EnemyState.patrolling;
+
+    //Patrol Related Variables
+    public Transform patrol1, patrol2;
+    Transform patrolTarget;
+
+    //Public Variables
+    public float accelerationSpeed, maxSpeed, attackRate, shotSpeed;
+    public GameObject projectile;
+
+    //Local Variables
+    float attackTimer;
+    Rigidbody2D rb;
+    GameObject player;
+
 	void Start () {
-		
+        patrolTarget = patrol1;
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+        if (currentState == EnemyState.patrolling)
+        {
+            #region Patrolling
+            if (patrolTarget.position.x > transform.position.x)
+            {
+                if (rb.velocity.x < maxSpeed)
+                {
+                    rb.AddForce(new Vector2(accelerationSpeed, 0));
+                }
+                else
+                {
+                    rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+                }              
+            }
+            else
+            {
+                if (rb.velocity.x > -maxSpeed)
+                {
+                    rb.AddForce(new Vector2(-accelerationSpeed, 0));
+                }
+                else
+                {
+                    rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+                }
+            }
+            #endregion
+        }
+        else if(currentState == EnemyState.aggressive)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= attackRate)
+            {
+                GameObject bullet = Instantiate(projectile, (Vector2)transform.position - new Vector2(0.5f, 0.5f), Quaternion.identity);
+                Vector2 directionVector = player.transform.position - transform.position;
+                directionVector.Normalize();
+
+                bullet.GetComponent<BulletScript>().velocity = directionVector * shotSpeed;
+
+                attackTimer = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        print("Collided");
+
+        if(col.gameObject.tag == "PatrolPoint")
+        {
+            if (patrolTarget == patrol1)
+            {
+                patrolTarget = patrol2;
+            }
+            else
+            {
+                patrolTarget = patrol1;
+            }
+        }   
+    }
 }
---
+++ GameJam/Assets/Scripts/Enemy/SetAggression.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetAggression : MonoBehaviour {
+
+    EnemyBehaviourScript ebs;    
+	void Start () {
+		ebs = transform.parent.GetComponent<EnemyBehaviourScript>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            ebs.currentState = EnemyBehaviourScript.EnemyState.aggressive;
+        }   
+    }
+}
