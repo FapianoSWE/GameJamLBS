c0068fe66eb2624981014cdc9e131feadf51eff2
FapianoSWE <33018606+FapianoSWE@users.noreply.github.com>
2018-05-19 08:35:15 +0200

    asdas

--- GameJam/Assets/Scripts/GameEntityScript.cs
+++ GameJam/Assets/Scripts/GameEntityScript.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class GameEntityScript : MonoBehaviour {
-
+    EventSystem eventSystem;
     GameObject optionsMenu;
     Slider volumeSlider;
+    public GameObject[] buttons;
 
     void Start()
     {
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
         volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
         optionsMenu = GameObject.Find("Options");
         optionsMenu.SetActive(false);
@@ -18,8 +21,19 @@ public class GameEntityScript : MonoBehaviour {
 
     void Update()
     {
-        
-        
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if(buttons[i] == eventSystem.currentSelectedGameObject)
+            {
+                eventSystem.currentSelectedGameObject.GetComponent<Image>().color = Color.blue;
+                eventSystem.currentSelectedGameObject.GetComponentInChildren<Text>().color = Color.white;
+            }
+            else
+            {
+                buttons[i].GetComponent<Image>().color = Color.white;
+                buttons[i].GetComponentInChildren<Text>().color = Color.black;
+            }
+        }
         DontDestroyOnLoad(gameObject);
     }
 
---
+++ GameJam/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    int health = 3;
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+
+	void Update ()
+    {
+
+	}
+}
